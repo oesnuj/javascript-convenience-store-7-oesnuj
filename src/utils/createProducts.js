746ecf93df73f0520edca5c5181641635e6c13d8
Junseo Kim <semiwest2@gmail.com>
2024-11-11 03:32:01 +0900

    refactor: 상품 데이터 파일 읽기 및 파싱하여 객체 리스트 반환 기능 추가

--- src/utils/createProducts.js
+++ src/utils/createProducts.js
@@ -1,27 +1,62 @@
 import { readFile } from 'fs/promises';
-import  Product from '../Convenience/Product.js';
+import Product from '../Convenience/Product/Product.js';
 
-export async function createProducts(filepath){
-  try{
-    const itemList = await readFile(filepath, 'utf-8');
-    const parsedItemList = parseItemList(itemList);
+export async function createProducts(filepath) {
+  const itemList = await readFile(filepath, 'utf-8');
+  const parsedItemList = parseItemList(itemList);
 
-    return parsedItemList.map((item) => {
-      const [name, price, quantity, promotionInfo] = item;
-      return new Product({name, price, quantity, promotionInfo});
-    })
+  const productMap = new Map();
 
-  }catch(error){
-    throw new Error(`[ERROR] ${error.message}`)
-  }
+  parsedItemList.forEach((item) => {
+    const [name, price, quantity, promotionInfo] = item;
+    const key = `${name}-${price}`;
+
+    if (productMap.has(key)) {
+      updateExistingProduct(productMap, key, quantity, promotionInfo);
+    } else {
+      addNewProduct(productMap, key, name, price, quantity, promotionInfo);
+    }
+  });
+
+  return Array.from(productMap.values());
+}
+
+function parseItemList(itemList) {
+  return itemList.trim().split('\n').slice(1).map(item => item.trim().split(','));
+}
+
+function updateExistingProduct(productMap, key, quantity, promotionInfo) {
+  const existingProduct = productMap.get(key);
+
+  const { defaultQuantity, promotionQuantity } = calculateQuantities(quantity, promotionInfo);
+  existingProduct.defaultQuantity += defaultQuantity;
+  existingProduct.promotionQuantity += promotionQuantity;
+}
+
+function addNewProduct(productMap, key, name, price, quantity, promotionInfo) {
+  const { defaultQuantity, promotionQuantity } = calculateQuantities(quantity, promotionInfo);
+
+  productMap.set(key, new Product({
+    name,
+    price: Number(price),
+    defaultQuantity,
+    promotionQuantity,
+    promotionInfo : promotionInfo === 'null' ? null : promotionInfo,
+  }));
 }
 
 
-function parseItemList(itemList){
-  const parsedItemList = itemList
-  .trim()
-  .split('\n')
-  .slice(1)
-  .map(item => item.trim().split(','));
-  return parsedItemList;
+function calculateQuantities(quantity, promotionInfo) {
+  let defaultQuantity = 0;
+  let promotionQuantity = 0;
+
+  if (promotionInfo && promotionInfo !== 'null') {
+    promotionQuantity = Number(quantity);
+  }
+
+  if (!promotionInfo || promotionInfo === 'null') {
+    defaultQuantity = Number(quantity);
+  }
+
+  return { defaultQuantity, promotionQuantity };
 }
