372753deaf9b4903bf5b06e2bf0a4c2157247eb8
Junseo Kim <semiwest2@gmail.com>
2024-11-11 04:35:59 +0900

    feat: InputView 입력 뷰 클래스 생성

---
+++ src/io/InputView.js
@@ -0,0 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+import { PROMPT_MESSAGES } from '../constants/messages.js';
+
+class InputView {
+  async getProductSelection() {
+    return await Console.readLineAsync(PROMPT_MESSAGES.INPUT.PRODUCT_SELECTION);
+  }
+
+  async getPromotionProductAddition(productName) {
+    return await Console.readLineAsync(PROMPT_MESSAGES.INPUT.PROMOTION_PRODUCT_ADDITION(productName));
+  }
+
+  async getPurchaseWithoutPromotion(productName, productQuantity) {
+    return await Console.readLineAsync(PROMPT_MESSAGES.INPUT.PURCHASE_WITHOUT_PROMOTION(productName, productQuantity));
+  }
+
+  async getMembershipDiscount() {
+    return await Console.readLineAsync(PROMPT_MESSAGES.INPUT.MEMBERSHIP_DISCOUNT);
+  }
+
+  async getAdditionalPurchase() {
+    return await Console.readLineAsync(PROMPT_MESSAGES.INPUT.ADDITIONAL_PURCHASE);
+  }
+}
+
+export default InputView;
