862d64c74becd270a2f40bf5e22522e9c7535ecf
Junseo Kim <semiwest2@gmail.com>
2024-11-11 22:45:58 +0900

    fix: 불필요 파일 삭제

--- src/constants/convenienceConfig.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const convenienceConfig = Object.freeze({
-  PRODUCT_INFO_PATH : path.resolve(__dirname, 'public/products.md'),
-  PROMOTION_INFO_PATH : path.resolve(__dirname, 'public/promotions.md'),
-});
