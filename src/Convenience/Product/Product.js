faf48b3247730c9d5369000aa37861531740b20a
Junseo Kim <semiwest2@gmail.com>
2024-11-11 21:02:58 +0900

    feat: Product 클래스에 재고 관리 기능 추가

--- src/Convenience/Product/Product.js
+++ src/Convenience/Product/Product.js
@@ -3,13 +3,46 @@ export default class Product {
   #price;
   #defaultQuantity;
   #promotionQuantity
-  #promotionInfo;
+  #promotionName;
 
-  constructor({name, price, defaultQuantity, promotionQuantity, promotionInfo}) {
+  constructor({name, price, defaultQuantity, promotionQuantity, promotionName}) {
     this.#name = name;
     this.#price = Number(price);
     this.#defaultQuantity = Number(defaultQuantity);
     this.#promotionQuantity = Number(promotionQuantity);
-    this.#promotionInfo = promotionInfo;
+    this.#promotionName = promotionName;
+
   }
-}
+
+  addToDefaultQuantity(quantity) {
+    this.#defaultQuantity += quantity;
+  }
+
+  addToPromotionQuantity(quantity) {
+    this.#promotionQuantity += quantity;
+  }
+
+  isPromotionStockSufficient(purchaseQuantity){
+    return purchaseQuantity <= this.#promotionQuantity;
+  }
+
+  removeFromDefaultQuantity(quantity) {
+    this.#defaultQuantity = Math.max(0, this.#defaultQuantity - quantity);
+  }
+
+  removeFromPromotionQuantity(quantity) {
+    this.#promotionQuantity = Math.max(0, this.#promotionQuantity - quantity);
+  }
+
+  getInfo() {
+    return {
+      name: this.#name,
+      price: this.#price,
+      defaultQuantity: this.#defaultQuantity,
+      promotionQuantity: this.#promotionQuantity,
+      promotionName: this.#promotionName,
+    };
+  };
+
+
+  }
