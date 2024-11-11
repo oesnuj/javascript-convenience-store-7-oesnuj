d5aaa487fd7a227606971d409fbd5994440042bc
Junseo Kim <semiwest2@gmail.com>
2024-11-11 20:58:55 +0900

    feat: 에러 메세지 상수화

---
+++ src/constants/error.js
@@ -0,0 +1,10 @@
+const ERRORS = Object.freeze({
+  INVALID_FORMAT : '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
+  PRODUCT_NOT_FOUND : '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.',
+  EXCEEDS_STOCK : '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+  INVALID_INPUT : '[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.',
+  INVALID_PRODUCT_QUANTITY: '[ERROR] 재고 수량은 양의 정수여야 합니다. 다시 입력해 주세요.',
+});
+
+
+export default ERRORS;
