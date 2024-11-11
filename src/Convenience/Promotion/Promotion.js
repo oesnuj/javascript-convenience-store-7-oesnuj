177672014890565b11b612934c5279abb22cbd75
Junseo Kim <semiwest2@gmail.com>
2024-11-11 21:10:27 +0900

    feat: Promotion 클래스 수정 및 새로운 메서드 추가
    - Promotion 클래스에 새로운 메서드 추가:
      - `isActive()`: 현재 날짜를 기준으로 프로모션이 활성화되어 있는지 확인
      - `calculateFreeItems(quantity)`: 주어진 수량에 대해 무료 제공 가능 여부 계산
      - `canReceiveOneMoreFree(quantity)`: 1개를 추가할 경우 무료 혜택을 받을 수 있는지 확인

--- src/Convenience/Promotion/Promotion.js
+++ src/Convenience/Promotion/Promotion.js
@@ -1,3 +1,5 @@
+import { DateTimes } from '@woowacourse/mission-utils'
+
 export default class Promotion {
   #name;
   #buy;
@@ -7,24 +9,30 @@ export default class Promotion {
 
   constructor({name, buy, get, start_date, end_date}) {
     this.#name = name;
-    this.#buy = buy;g
-    this.#get = get;
+    this.#buy = Number(buy);
+    this.#get = Number(get);
     this.#start_date = new Date(start_date);
     this.#end_date = new Date(end_date);
   }
 
+  getName(){
+    return this.#name;
+  }
+
+
   isActive(){
-    const currentDate = new Date();
+    const currentDate = DateTimes.now();
     return currentDate >= this.#start_date && currentDate <= this.#end_date;
   }
 
-  calculateBonusProducts(buyProductQuantity){
-    if(buyProductQuantity < this.#buy){
-      return 0;
-    }
-    return buyProductQuantity / this.#buy
-
+  // 주어진 수량으로 무료 제공 가능 여부 확인
+  calculateFreeItems(quantity) {
+    return Math.floor(quantity / (this.#buy + this.#get));
   }
 
+  // 1개 추가 시 무료 혜택을 받을 수 있는지 확인
+  canReceiveOneMoreFree(quantity) {
+    return quantity % (this.#buy + this.#get) === this.#buy;
+  }
 
 }
